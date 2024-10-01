4d8669ecc19eac52ea96b18195ec9368c69e8c3b
Kieran Caraway <kierancaraway20@yahoo.com>
2024-10-01 03:08:21 -0500

    completion
    added checks for the following:
    horizontal 4 in a row
    vertical 4 in a row
    anti diagonal
    diagonal

--- js/connect4.js
+++ js/connect4.js
@@ -65,6 +65,7 @@ function setPiece() {
 };
 
 function checkWinner() {
+    //* horizontal check
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns - 3; c++) {
             if (board[r][c] != ' ') {
@@ -75,6 +76,41 @@ function checkWinner() {
             }
         }
     }
+    //* vertical check
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                    setWinner(r, c)
+                    return;
+                }
+            }
+        }
+    };
+
+    //* anti diagonal check
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != " ") {
+                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    //* diagonal check
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != " ") {
+                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    };
 };
 
 function setWinner(r, c) {
